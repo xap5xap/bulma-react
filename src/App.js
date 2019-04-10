d8bbbcc6d671c7dfa15588ddbda0c79fbba483a1
Xavier Perez <xap5xap@users.noreply.github.com>
2019-04-10 15:31:06 -0500

    add bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <h1 className="title">Bulma</h1>
+
+        <p className="subtitle">
+          Modern CSS framework based on{' '}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+            Flexbox
           </a>
-        </header>
+        </p>
+
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Input" />
+          </div>
+        </div>
+
+        <div className="field">
+          <p className="control">
+            <span className="select">
+              <select>
+                <option>Select dropdown</option>
+              </select>
+            </span>
+          </p>
+        </div>
+
+        <div className="buttons">
+          <a className="button is-primary">Primary</a>
+          <a className="button is-link">Link</a>
+        </div>
       </div>
     );
   }
